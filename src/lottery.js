0eb729a35c800fb5c3ae0015931dbaa66a256ec8
kan.k <kan.k@siriustech.io>
2022-07-04 15:26:21 +0700

    Initial Commit

---
+++ src/lottery.js
@@ -0,0 +1,57 @@
+import web3 from './web3'
+
+const address = '0xcbF727d789163300381b5fEDA2B0F8097E770C72'
+
+const abi = [
+	{
+		constant: true,
+		inputs: [],
+		name: 'manager',
+		outputs: [{ name: '', type: 'address' }],
+		payable: false,
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		constant: false,
+		inputs: [],
+		name: 'pickWinner',
+		outputs: [],
+		payable: false,
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		constant: true,
+		inputs: [],
+		name: 'getPlayers',
+		outputs: [{ name: '', type: 'address[]' }],
+		payable: false,
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		constant: false,
+		inputs: [],
+		name: 'enter',
+		outputs: [],
+		payable: true,
+		stateMutability: 'payable',
+		type: 'function',
+	},
+	{
+		constant: true,
+		inputs: [{ name: '', type: 'uint256' }],
+		name: 'players',
+		outputs: [{ name: '', type: 'address' }],
+		payable: false,
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		payable: false,
+		stateMutability: 'nonpayable',
+		type: 'constructor',
+	},
+]
